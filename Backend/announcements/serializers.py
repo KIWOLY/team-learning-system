2ce965784b347a58b9dc36e113d5b865b39391b6
Habrovan004 <habrovan14@gmail.com>
2025-11-17 13:45:41 +0300

    Add truncated content to Announcement serializer

--- Backend/announcements/serializers.py
+++ Backend/announcements/serializers.py
@@ -3,13 +3,21 @@ from .models import Announcement
 
 class AnnouncementSerializer(serializers.ModelSerializer):
     author_name = serializers.SerializerMethodField()
+    short_content = serializers.SerializerMethodField()  # Truncated content field
 
     class Meta:
         model = Announcement
-        fields = ['id', 'title', 'content', 'date', 'author_name']
+        # Ongeza short_content kwenye fields
+        fields = ['id', 'title', 'content', 'short_content', 'date', 'author_name']
 
     def get_author_name(self, obj):
         author = getattr(obj, 'author', None)
         if not author:
             return None
         return getattr(author, 'username', str(author))
+
+    def get_short_content(self, obj):
+        """Return content truncated to 50 characters"""
+        if len(obj.content) > 50:
+            return obj.content[:50] + "..."
+        return obj.content
